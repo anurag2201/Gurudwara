ba6508b6e61ac9efac069f8cc2271a2a86a52a64
anurag2201 <anurag2201@gmail.com>
2015-09-11 11:41:13 +0530

    guru view not found removed

--- www/js/guru-view.js
+++ www/js/guru-view.js
@@ -17,7 +17,7 @@ gd.controller('homeCTRL', function($scope, $http, $filter) {
             $scope.selected = found[0].value;
             $scope.img = found[0].img;
         } else {
-            $scope.selected = 'Not found';
+            $scope.selected = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
             $scope.img = "img/guru/picture/guru-gobind.jpg";
         }
         return $scope.selected;
